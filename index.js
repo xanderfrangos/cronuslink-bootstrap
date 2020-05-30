2b290451e8565606e91e74a697d8440252486cd0
Xander Frangos <33106561+xanderfrangos@users.noreply.github.com>
2020-05-30 17:22:09 -0400

    CORS fix

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require("path")
 const app = express();
+const cors = require('cors')
 
-app.use(require('cors'));
+app.use(cors());
 app.use('/', express.static(path.join(__dirname, "./docs/")))
 app.listen(3004)
 
